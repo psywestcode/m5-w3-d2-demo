9db7bc1a2737a99cc31423a0edf0c894ea45cadf
Sailesh Mohapatra <s.mohapatra5530@westcliff.edu>
2026-03-22 11:49:43 -0700

    createlist + app edits

--- crud-json-server/src/App.js
+++ crud-json-server/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Lists from "./Lists";
+import CreateList from "./CreateList";
+
 
 class App extends React.Component {
     constructor(props) {
@@ -26,6 +28,38 @@ class App extends React.Component {
       .catch(console.log);
    }
 
+  handleChange = (event) => {
+  let title = this.state.singledata.title;
+  let author = this.state.singledata.author;
+
+  if (event.target.name === "title") title = event.target.value;
+  else author = event.target.value;
+
+  this.setState({
+    singledata: {
+      title: title,
+      author: author
+    }
+  });
+}
+
+createList = () => {
+  fetch("http://localhost:5000/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(this.state.singledata)
+  }).then(
+    this.setState({
+      singledata: {
+        title: "",
+        author: ""
+      }
+    })
+  );
+}
+
    render() {
     const listTable = this.state.loading ? (
       <span>Loading Data...... Please be patient</span>
@@ -36,10 +70,18 @@ class App extends React.Component {
     return (
       <div className="container">
         <span className="title-bar">
-          <button type="button" className="btn btn-primary" onClick={this.getLists}>Get Lists</button>
-        </span>
+            <button type="button" className="btn btn-primary" onClick={this.getLists}>
+            Get Lists
+          </button>
+          <CreateList
+            singledata={this.state.singledata}
+            handleChange={this.handleChange}
+            createList={this.createList}
+  />
+</span>
         {listTable}
       </div>
+      
     );
   }
 
